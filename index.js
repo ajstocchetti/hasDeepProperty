445167b7479ba1651775c8b8df1aa33f0d468b2b
tackypinkflamingos <ajstocchetti@gmail.com>
2017-01-01 15:12:48 -0500

    has deep property function

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function hasDeepProperty(obj, propArray) {
+    propArray = (Array.isArray(propArray)) ? propArray : [propArray];
+
+    if(!propArray.length) return true; // no more properties to check
+    if(obj.hasOwnProperty(propArray[0])) {
+        const nextObj = obj[propArray[0]];
+        const nextList = propArray.slice(1, propArray.length);
+        return hasDeepProperty(nextObj, nextList);
+    } else return false;
+}
+
+module.exports = {
+    hasDeepProperty,
+};
